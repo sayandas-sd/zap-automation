ce61d2d7874671c3d07feb09e3909e22fe297f5b
sayandas-sd <sayandasmac@gmail.com>
2024-11-22 20:20:56 +0530

    create taskRun in post endpoint

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,10 +1,31 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+
 const port = 3000
 const app = express();
+const prisma = new PrismaClient();
 
 
-app.post("/webhook", (req, res) => {
-    res.send("hello");
+app.post("/hooks/catch/:userId/:taskId", async (req, res) => {
+    const body = req.body;
+    const userId = req.params.userId;
+    const taskId = req.params.taskId;
+    
+    await prisma.$transaction(async tx => {
+        const taskRun = await prisma.taskRun.create({
+            data: {
+                taskId: taskId,
+                metadata: body
+            }
+        });
+
+        await prisma.taskRunOut.create({
+            data: {
+                taskRunId: taskRun.id
+            }
+        })
+    })
+
 })
 
 app.listen(port, () => {
