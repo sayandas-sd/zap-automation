c8f98c996320456c94afa4c9771c865f9e6fbe12
sayandas-sd <sayandasmac@gmail.com>
2024-11-23 12:29:34 +0530

    added kafka producer

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,10 +1,27 @@
 import { PrismaClient } from '@prisma/client'
+import { Kafka } from "kafkajs";
+
 const worker = new PrismaClient();
 
+const kafka = new Kafka({
+    clientId: 'outbox_worker',
+    brokers: ['localhost:9092']
+  })
+
 async function main() {
+
+    const producer = kafka.producer()
+    await producer.connect()
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+
     while(1) {
         const pendingData = await worker.taskRunOut.findMany({
-            
+            where:{},
+            take: 10
+        })
+
+        pendingData.forEach(r => {
+
         })
     }
 }
