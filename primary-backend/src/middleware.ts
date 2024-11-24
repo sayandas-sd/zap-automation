150857813c1b959a5827b806e0e7fb123fad1b2a
sayandas-sd <sayandasmac@gmail.com>
2024-11-24 12:42:45 +0530

    added middleware jwt token

--- primary-backend/src/middleware.ts
+++ primary-backend/src/middleware.ts
@@ -1,5 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { JWT_PASSWORD } from "./config";
+import jwt from "jsonwebtoken";
+import { string } from "zod";
+
 
 export function authMiddleware(req:Request, res:Response, next:NextFunction) {
+    const token = req.headers.authorization as unknown as string;
 
+    try{
+        const payload = jwt.verify(token, JWT_PASSWORD);
+        //@ts-ignore
+        req.id = payload.id;
+        next();
+    } catch(e) {
+        res.status(403).json({
+            msg: "you are not logged in"
+        })
+    }
 }
